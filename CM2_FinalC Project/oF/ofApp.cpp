05be6d99ead5717357afb9339582d12cf5a625d9
santa-sukitoku <santanaruse@gmail.com>
2019-12-20 00:16:40 +0900

    openFrameworksのソースコードを追加

---
+++ CM2_FinalC Project/oF/ofApp.cpp
@@ -0,0 +1,102 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofBackground(0);
+	ofSetCircleResolution(64);
+	ofSetBackgroundAuto(false);
+	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	color = 100;
+
+	// 指定したIPアドレスとポート番号でサーバーに接続
+	sender.setup(HOST, PORT);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	ofSetColor(ofRandom(mouseX) / 2, ofRandom(mouseY) / 2, ofRandom(1, 255) * 3, 70);
+	ofDrawCircle(mouseX, mouseY, ofRandom(5, 8));
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+	// OSCMessageの準備
+	ofxOscMessage m;
+
+	// OSCアドレスの指定
+	m.setAddress("/mouse/position");
+	
+	// OSC引数として現在のマウスの座標(x, y)を送信
+	m.addIntArg(x);
+	m.addIntArg(y);
+
+	// OSCMessageを送信
+	sender.sendMessage(m);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+	ofxOscMessage m;
+	m.setAddress("/mouse/button");
+	m.addIntArg(1);
+	sender.sendMessage(m);
+
+	ofSetColor(ofRandom(mouseX) / 2, ofRandom(mouseY) / 2, ofRandom(1, 255) * 3, 95);
+	ofDrawCircle(mouseX, mouseY, ofRandom(30, 40));
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+	ofxOscMessage m;
+	m.setAddress("/mouse/button");
+	m.addIntArg(0);
+	sender.sendMessage(m);
+
+	ofSetColor(ofRandom(mouseX) / 2, ofRandom(mouseY) / 2, ofRandom(1, 255) * 3, 95);
+	ofDrawCircle(mouseX, mouseY, ofRandom(30, 40));
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
